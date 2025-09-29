8352dccb05ca57f9d6ce5dd34db0351c16c0d354
Mehdi <mehdi.boudakhani@proton.me>
2025-09-29 23:09:25 +0200

    Setting up dependency injection.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+class Program
+{
+    static async Task Main()
+    {
+        await Task.Delay(-1);
+    }
+
+    private ServiceProvider ConfigureServices()
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+        {
+            GatewayIntents = GatewayIntents.AllUnprivileged
+                             | GatewayIntents.GuildMembers
+                             | GatewayIntents.GuildVoiceStates
+                             | GatewayIntents.MessageContent
+        }));
+        return services.BuildServiceProvider();
+    }
+}
