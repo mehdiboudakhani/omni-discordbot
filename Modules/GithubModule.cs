24a3555dd6948c394fb25a38c3e46b0dd57eb1f2
Mehdi <mehdi.boudakhani@proton.me>
2025-09-30 15:42:49 +0200

    Improved display of GitHub query results.

--- Modules/GithubModule.cs
+++ Modules/GithubModule.cs
@@ -12,24 +12,24 @@
 
         [SlashCommand("profile", "Viewing a GitHub user's profile.")]
         public async Task UserAsync(string username) =>
-            await GithubRequestAsync($"users/{username}", EmbedHelper.GithubProfileEmbed);
+            await GithubRequestAsync($"users/{username}", EmbedHelper.GithubProfile);
 
         [SlashCommand("repository", "Viewing a user's GitHub repository.")]
         public async Task RepositoryAsync(string owner, string repository) =>
-            await GithubRequestAsync($"repos/{owner}/{repository}", EmbedHelper.GithubRepositoryEmbed);
+            await GithubRequestAsync($"repos/{owner}/{repository}", EmbedHelper.GithubRepository);
 
         [SlashCommand("last-issues", "Displaying the last 5 open issues of a GitHub repository.")]
         public async Task IssuesAsync(string owner, string repository) =>
             await GithubRequestAsync(
                 $"repos/{owner}/{repository}/issues?state=open&per_page=5",
-                root => EmbedHelper.GithubLastIssuesRepositoryEmbed(root, owner, repository)
+                root => EmbedHelper.GithubLastIssues(root, owner, repository)
             );
 
         [SlashCommand("last-commits", "Displaying the last 5 commits of a GitHub repository.")]
         public async Task CommitsAsync(string owner, string repository) =>
             await GithubRequestAsync(
                 $"repos/{owner}/{repository}/commits?per_page=5", 
-                root => EmbedHelper.GithubLastCommitsRepositoryEmbed(root, owner, repository)
+                root => EmbedHelper.GithubLastCommits(root, owner, repository)
             );
 
         private async Task GithubRequestAsync(string url, Func<JsonElement, Embed> embedFactory)
@@ -39,7 +39,7 @@
                 var response = await _httpClient.GetAsync(url);
                 if (!response.IsSuccessStatusCode)
                 {
-                    await RespondAsync(embed: EmbedHelper.ErrorEmbed("Unable to find the requested resource on GitHub."), ephemeral: true);
+                    await RespondAsync(embed: EmbedHelper.Error("Unable to find the requested resource on GitHub."), ephemeral: true);
                     return;
                 }
                 var content = await response.Content.ReadAsStringAsync();
@@ -47,7 +47,7 @@
             }
             catch (Exception)
             {
-                await RespondAsync(embed: EmbedHelper.ErrorEmbed("A technical problem occurred while contacting GitHub."), ephemeral: true);
+                await RespondAsync(embed: EmbedHelper.Error("A technical problem occurred while contacting GitHub."), ephemeral: true);
             }
         }
     }
--- Utils/EmbedHelper.cs
+++ Utils/EmbedHelper.cs
@@ -1,71 +1,70 @@
 ﻿namespace Omni.Utils
 {
-    internal class EmbedHelper
+    class EmbedHelper
     {
-        public static Embed ErrorEmbed(string reason)
+        internal static Embed Error(string reason)
         {
             var embedBuilder = new EmbedBuilder()
                 .WithTitle("Erreur")
                 .WithDescription("An error has occured.\n> **Reason** : " + reason)
-                .WithColor(Color.Red)
-                .WithFooter("This message will be deleted automatically.");
+                .WithColor(Color.Red);
             return embedBuilder.Build();
         }
 
-        public static Embed GithubProfileEmbed(JsonElement user)
+        internal static Embed GithubProfile(JsonElement user)
         {
-            var name = user.TryGetProperty("name", out var nameProp) ? nameProp.GetString() : null;
             var bio = user.TryGetProperty("bio", out var bioProp) ? bioProp.GetString() : null;
             var embedBuilder = new EmbedBuilder()
-                .WithTitle($"GitHub profile : {user.GetProperty("login").GetString()}")
+                .WithTitle($"👤 GitHub profile : {user.GetProperty("login").GetString()}")
+                .WithDescription(user.GetProperty("bio").GetString())
+                .AddField("Account created", DateTime.Parse(user.GetProperty("created_at").GetString()!).ToString("dd-MM-yyyy"))
+                .AddField("Public repositories", user.GetProperty("public_repos").GetInt32())
                 .WithUrl(user.GetProperty("html_url").GetString())
                 .WithThumbnailUrl(user.GetProperty("avatar_url").GetString())
                 .WithColor(Color.Green);
-            if (!string.IsNullOrWhiteSpace(name))
-                embedBuilder.AddField("Nom", name, true);
-            if (!string.IsNullOrWhiteSpace(bio))
-                embedBuilder.AddField("Bio", bio, true);
             return embedBuilder.Build();
         }
 
-        public static Embed GithubRepositoryEmbed(JsonElement repository)
+        internal static Embed GithubRepository(JsonElement repository)
         {
             var embedBuilder = new EmbedBuilder()
-                .WithTitle($"GitHub repository : {repository.GetProperty("owner").GetProperty("login").GetString()}/{repository.GetProperty("name").GetString()}")
+                .WithTitle($"📦 GitHub repository : {repository.GetProperty("owner").GetProperty("login").GetString()}/{repository.GetProperty("name").GetString()}")
                 .WithUrl(repository.GetProperty("html_url").GetString())
                 .WithDescription(repository.GetProperty("description").GetString())
                 .WithColor(Color.Green)
-                .AddField("Dernière mise à jour", repository.GetProperty("updated_at").GetString())
-                .AddField("Issues ouvertes", repository.GetProperty("open_issues_count").GetInt32());
+                .AddField("Last update", DateTime.Parse(repository.GetProperty("updated_at").GetString()!).ToString("dd-MM-yyyy"), true)
+                .AddField("Open issues", repository.GetProperty("open_issues_count").GetInt32(), true);
             return embedBuilder.Build();
         }
 
-        public static Embed GithubLastIssuesRepositoryEmbed(JsonElement issues, string owner, string repository)
+        internal static Embed GithubLastIssues(JsonElement issues, string owner, string repository)
         {
             var embedBuilder = new EmbedBuilder()
-                .WithTitle($"5 last open issues : {owner}/{repository}")
+                .WithTitle($"🐛 5 last open issues : {owner}/{repository}")
                 .WithUrl($"https://github.com/{owner}/{repository}/issues")
                 .WithColor(Color.Green);
-            int count = 0;
             foreach (var issue in issues.EnumerateArray())
             {
-                if (count++ >= 5) break;
-                embedBuilder.AddField($"{issue.GetProperty("number").GetInt32()} - {issue.GetProperty("title").GetString()}", $"{issue.GetProperty("user").GetProperty("login").GetString()} * [Voir l'issue]({issue.GetProperty("html_url").GetString})");
+                embedBuilder.AddField(
+                    $"{issue.GetProperty("number").GetInt32()} • {issue.GetProperty("title").GetString()}", 
+                    $"{issue.GetProperty("user").GetProperty("login").GetString()} • {DateTime.Parse(issue.GetProperty("created_at").GetString()!).ToString("dd/MM/yyyy")} • [View issue]({issue.GetProperty("html_url").GetString()})"
+                );
             }
             return embedBuilder.Build();
         }
 
-        public static Embed GithubLastCommitsRepositoryEmbed(JsonElement commits, string owner, string repository)
+        internal static Embed GithubLastCommits(JsonElement commits, string owner, string repository)
         {
             var embedBuilder = new EmbedBuilder()
-                .WithTitle($"5 last open commits : {owner}/{repository}")
+                .WithTitle($"📝 5 last open commits : {owner}/{repository}")
                 .WithUrl($"https://github.com/{owner}/{repository}/commits")
                 .WithColor(Color.Green);
-            int count = 0;
             foreach (var commit in commits.EnumerateArray())
             {
-                if (count++ >= 5) break;
-                embedBuilder.AddField($"{commit.GetProperty("commit").GetProperty("author").GetProperty("name").GetString()}", $"[{commit.GetProperty("commit").GetProperty("message").GetString()?.Split('\n')[0]}({commit.GetProperty("html_url").GetString()})]");
+                embedBuilder.AddField(
+                    $"{commit.GetProperty("commit").GetProperty("author").GetProperty("name").GetString()} • {commit.GetProperty("commit").GetProperty("author").GetProperty("date").GetDateTime().ToString("dd/MM/yyyy HH:mm:ss")}", 
+                    $"{commit.GetProperty("commit").GetProperty("message").GetString()} • [View commit]({commit.GetProperty("html_url").GetString()})"
+                );
             }
             return embedBuilder.Build();
         }
