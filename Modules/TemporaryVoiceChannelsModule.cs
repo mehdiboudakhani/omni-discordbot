a848c906acf7d036fb667fcdc2302776039462a4
Mehdi <mehdi.boudakhani@proton.me>
2025-10-10 21:37:01 +0200

    Add temporary voice channels module.

---
+++ Modules/TemporaryVoiceChannelsModule.cs
@@ -0,0 +1,63 @@
+﻿namespace Omni.Modules
+{
+    [Group("temporary-voice-channels", "Temporary voice channels management.")]
+    public class TemporaryVoiceChannelsModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        private readonly DiscordSocketClient _discordSocketClient;
+        private static readonly HashSet<ulong> _hubs = [];
+        private static readonly Dictionary<ulong, ulong> _temporaryChannels = [];
+
+        public TemporaryVoiceChannelsModule(DiscordSocketClient discordSocketClient)
+        {
+            _discordSocketClient = discordSocketClient;
+            _discordSocketClient.UserVoiceStateUpdated += HandleTemporaryVoiceChannelsAsync;
+        }
+
+        [SlashCommand("add-hub", "Add a hub.")]
+        public async Task AddHubAsync(IVoiceChannel channel)
+        {
+            if (_hubs.Add(channel.Id))
+                await RespondAsync($"Hub {channel.Name} added.", ephemeral: true);
+            else
+                await RespondAsync($"{channel.Name} is already a hub.", ephemeral: true);
+        }
+
+        [SlashCommand("remove-hub", "Remove a hub.")]
+        public async Task RemoveHubAsync(IVoiceChannel channel)
+        {
+            if (_hubs.Remove(channel.Id))
+                await RespondAsync($"Hub {channel.Name} removed.", ephemeral: true);
+            else
+                await RespondAsync($"{channel.Name} is not a hub.", ephemeral: true);
+        }
+
+        [SlashCommand("list-hubs", "List hubs.")]
+        public async Task ListHubsAsync()
+        {
+            await RespondAsync($"Hubs:\n" + string.Join("\n", _hubs.Select(id => $"<#{id}>")), ephemeral: true);
+        }
+
+        private async Task HandleTemporaryVoiceChannelsAsync(SocketUser user, SocketVoiceState before, SocketVoiceState after)
+        {
+            if (user is not SocketGuildUser guildUser) return;
+            if (after.VoiceChannel is not null && _hubs.Contains(after.VoiceChannel.Id) && !_temporaryChannels.ContainsKey(guildUser.Id))
+            {
+                var categoryIdentifier = after.VoiceChannel.CategoryId;
+                var temporaryChannel = await guildUser.Guild.CreateVoiceChannelAsync($"{guildUser.Username}'s channel", properties =>
+                {
+                    if (categoryIdentifier is not null)
+                        properties.CategoryId = categoryIdentifier;
+                });
+                _temporaryChannels[guildUser.Id] = temporaryChannel.Id;
+                await guildUser.ModifyAsync(properties => properties.Channel = temporaryChannel);
+            }
+            if (before.VoiceChannel is not null && _temporaryChannels.ContainsValue(before.VoiceChannel.Id) && before.VoiceChannel.ConnectedUsers.Count == 0)
+            {
+                await before.VoiceChannel.DeleteAsync();
+                var temporaryChannel = _temporaryChannels.FirstOrDefault(kvp => kvp.Value == before.VoiceChannel.Id);
+                if (!temporaryChannel.Equals(default(KeyValuePair<ulong, ulong>)))
+                    _temporaryChannels.Remove(temporaryChannel.Key);
+            }
+        }
+    }
+}
