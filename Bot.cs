f6427eb8dbd7a7f8ab64d6e459d15cb359ce8f9f
Mehdi <mehdi.boudakhani@proton.me>
2025-10-10 16:12:24 +0200

    Set up bot.

---
+++ Bot.cs
@@ -0,0 +1,45 @@
+﻿using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Omni
+{
+    class Bot
+    {
+        private readonly DiscordSocketClient _discordSocketClient;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly InteractionService _interactionService;
+
+        public Bot()
+        {
+            _discordSocketClient = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                GatewayIntents = GatewayIntents.AllUnprivileged
+                                 | GatewayIntents.GuildMembers
+                                 | GatewayIntents.GuildVoiceStates
+                                 | GatewayIntents.MessageContent
+            });
+            _serviceProvider = new ServiceCollection()
+                .AddSingleton(_discordSocketClient)
+                .AddSingleton(serviceProvider => new InteractionService(serviceProvider.GetRequiredService<DiscordSocketClient>()))
+                .BuildServiceProvider();
+            _interactionService = _serviceProvider.GetRequiredService<InteractionService>();
+        }
+
+        public async Task RunAsync()
+        {
+            _discordSocketClient.InteractionCreated += OnInteractionCreatedAsync;
+            _discordSocketClient.Ready += OnReadyAsync;
+            await _discordSocketClient.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("OMNI_DISCORD_BOT_TOKEN"));
+            await _discordSocketClient.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private async Task OnInteractionCreatedAsync(SocketInteraction socketInteraction) =>
+            await _interactionService.ExecuteCommandAsync(new SocketInteractionContext(_discordSocketClient, socketInteraction), _serviceProvider);
+
+        private async Task OnReadyAsync() =>
+            await _interactionService.RegisterCommandsToGuildAsync(ulong.Parse(Environment.GetEnvironmentVariable("OMNI_GUILD_IDENTIFIER")!));
+    }
+}
